7ab4ca7e9e6b5bc935491af2299fefa21de201a0
VanyaSupik <99318679+VanyaSupik@users.noreply.github.com>
2023-01-31 10:45:52 +0200

    Add files via upload

---
+++ Task2/Maine.java
@@ -0,0 +1,11 @@
+package Task2;
+
+public class Maine {
+    public static void main(String[] args) {
+        One o = new One();
+        o.test(1,3,2);
+        o.test(1.0,3.0,2.0);
+        o.test(1,3);
+        
+    }
+}
---
+++ Task2/One.java
@@ -0,0 +1,15 @@
+package Task2;
+
+public class One {
+
+    void test(int d, int h, int j){
+        System.out.println(d+h+j);
+    }
+    void test(double d, double h, double j){
+        System.out.println(d+h+j);
+    }
+    void test(int d, int h){
+        int j = 2;
+        System.out.println(d+h+j);
+    }
+}
